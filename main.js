a0aeb04fbd186a37a07ddf49b2bd868c4fc14faa
Merola Giuseppe <merolagiuseppe91@outlook.it>
2024-03-06 16:19:09 +0100

    creato file js per esercizio

---
+++ main.js
@@ -0,0 +1,50 @@
+// // Esercizio 1
+// let numero1 = 2;
+// let numero2 = 4;
+// let numero3 = 5;
+// let numero4 = 10;
+// let numero5 = 3;
+
+// let somma = numero1 + numero2 + numero3 + numero4 + numero5;
+// let media = somma / 5;
+
+// console.log(`La somma tra i numeri equivale a ${somma} e la media risulta ${media}`);
+
+
+
+// Esercizio 2
+// let annoCorrente = 2024;
+// let annoNascita = 1991;
+
+// let annoPersona = annoCorrente - annoNascita;
+// let anniMancanti = 100 - annoPersona;
+
+// console.log(`Hai ${annoPersona} anni e ti mancano ${anniMancanti} anni per compierne 100`);
+
+
+
+// // Esercizio 3
+// let numeroGatti = 44;
+// let gattiPerFila = 6;
+
+// let fileRisultanti = Math.round (numeroGatti / gattiPerFila);
+// let resto = numeroGatti % gattiPerFila;
+// let gattiMancanti = gattiPerFila - resto;
+
+// console.log(`Ci sono ${fileRisultanti} file di gatti e ne mancano ${gattiMancanti} per una nuova fila, con un avanzo di ${resto}`);
+
+
+
+// Esercizio Extra
+// let a = 10;
+// let b = -2;
+// let c = 31;
+// let d = 22;
+// let e = 15;
+// let f = -6;
+// let g = 7;
+
+// let temperaturaAlta = Math.max (a,b,c,d,e,f,g);
+// let temperaturaMinima = Math.min (a,b,c,d,e,f,g);
+
+// console.log(`La temperatura più calda è ${temperaturaAlta} quella più fredda ${temperaturaMinima}`);
