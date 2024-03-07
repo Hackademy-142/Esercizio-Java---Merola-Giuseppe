68a27769016f856e5e43bafe7443c4f2df8d2d50
Merola Giuseppe <merolagiuseppe91@outlook.it>
2024-03-07 18:15:57 +0100

    aggiunti gli esercizi risolti su condizioni e cicli

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Giorno 1 ------------------------------------------------
+
 // // Esercizio 1
 // let numero1 = 2;
 // let numero2 = 4;
@@ -48,3 +50,100 @@
 // let temperaturaMinima = Math.min (a,b,c,d,e,f,g);
 
 // console.log(`La temperatura più calda è ${temperaturaAlta} quella più fredda ${temperaturaMinima}`);
+
+
+// Giorno 2 -----------------------------------------------
+
+
+// Esercizio 1
+
+// let voto = 29;
+
+// if (voto >= 1 && voto < 18) {
+//     console.log("Bocciato");
+// } else if (voto >= 18 && voto <= 21){
+//     console.log("Sufficiente");
+// } else if (voto >= 21 && voto <= 24){
+//     console.log("Buono");
+// } else if (voto >= 24 && voto <= 27){
+//     console.log("Distinto");
+// } else if (voto >= 27 && voto <= 29){
+//     console.log("Ottimo");
+// } else if (voto > 31){
+//     console.log("Eccellente");
+// } else {
+//     console.log("Valore non valido");
+// }
+
+
+// Esercizio 2
+
+// let temperatura = -12 ;
+
+// if (temperatura < -10) {
+//     console.log("Copriti...ancora ti raffreddi");
+// } else if (temperatura < 0){
+//     console.log("Non è tanto il freddo quanto l'umidità");
+// } else if (temperatura < 20){
+//     console.log("Non ci sono più le mezze stagioni");
+// } else if (temperatura < 30){
+//     console.log("Mi dia una peroni sudata");
+// } else if (temperatura >= 30){
+//     console.log("Lu mare, lu sole, lu ientu");
+// } else {
+//     console.log("Valore non valido");
+// }
+
+
+// Esercizio 3
+
+// let bevanda;
+
+// do {
+
+//     bevanda = +prompt("Scegli la tua bevanda");
+   
+// } while (bevanda != 1 && bevanda != 2 && bevanda != 3);
+
+// switch (bevanda) {
+//     case 1:
+//     console.log("È stata selezionata l'acqua");
+//     break;
+
+//     case 2:
+//     console.log("È stata selezionata coca cola");
+//     break;
+
+//     case 3:
+//     console.log("È stata selezionata birra");
+//     break;
+
+//     default:
+//     console.log("Valore non valido");
+// }  
+
+
+//  Esercizio 4
+
+// let tiri = 5;
+// let punteggioGiocatore1 = 0;
+// let punteggioGiocatore2 = 0;
+
+
+// for(let i = 0; i < tiri; i++ ){
+
+//     let lancioGiocatore1 = Math.round(Math.random()* (6 - 1)+ 1);
+//     punteggioGiocatore1 = punteggioGiocatore1 + lancioGiocatore1;
+
+//     console.log(`Tiro ${i+1} Giocatore 1 - Punti: ${lancioGiocatore1}`)
+
+//     let lancioGiocatore2 = Math.round(Math.random()* (6 - 1)+ 1);
+//     punteggioGiocatore2 = punteggioGiocatore2 + lancioGiocatore2;
+
+//     console.log(`Tiro ${i+1} Giocatore 2 - Punti: ${lancioGiocatore2}`)
+// }
+
+
+// console.log("Totale Giocatore 1: " + punteggioGiocatore1);
+// console.log("Totale Giocatore 2: " + punteggioGiocatore2);
+
